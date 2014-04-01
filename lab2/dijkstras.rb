ecf2550c3ac4b0a306537c9f510897d3276e3dd8
Per-Victor Persson <dracyr@gmail.com>
2014-04-01 12:40:01 +0200

    Tidied up lab2, dijkstras

--- lab2/dijkstras.rb
+++ lab2/dijkstras.rb
@@ -21,22 +21,8 @@ def build_for node
   end
 end
 
-def build_tree
-  @tree = {}
-  @words.each do |w|
-    @tree[w] = []
-    @words.each do |compare|
-      contains = (compare.scan(/./).contains_all? w[1..4].scan(/./))
-      @tree[w] << compare if (w != compare) and contains
-    end
-  end
-  puts @tree if @debug
-  @tree
-end
-
 INFINITY = 1 << 32
 def traverse from, to
-  #puts "from: #{from}"
   @tree = {}
   visited = {}
   visited.default(false)
@@ -52,7 +38,6 @@ def traverse from, to
     node = pq.pop
     visited[node] = true
     edges = build_for node
-    #puts "edges: #{edges}"
     unless edges.empty?
       edges.each do |w|
         if shortest_distances[w] > shortest_distances[node] + 1
@@ -89,36 +74,3 @@ end
 @debug = false
 parse ARGV[0], ARGV[1]
 run
-
-
-require 'pqueue'
-
-class Algorithm
-  INFINITY = 1 << 32
-
-  def self.dijkstra(source, edges, weights, n)
-    visited = Array.new(n, false)
-    shortest_distances = Array.new(n, INFINITY)
-    previous = Array.new(n, nil)
-    pq = PQueue.new(proc {|x,y| shortest_distances[x] < shortest_distances[y]})
-
-    pq.push(source)
-    visited[source] = true
-    shortest_distances[source] = 0
-
-    while pq.size != 0
-      v = pq.pop
-      visited[v] = true
-      if edges[v]
-        edges[v].each do |w|
-          if !visited[w] and shortest_distances[w] > shortest_distances[v] + weights[v][w]
-            shortest_distances[w] = shortest_distances[v] + weights[v][w]
-            previous[w] = v
-            pq.push(w)
-          end
-        end
-      end
-    end
-    return [shortest_distances, previous]
-  end
-end
