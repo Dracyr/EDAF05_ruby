4ac28f8e6d73a9db21c0d0c6a1d799d35fd4c93b
Per-Victor Persson <dracyr@gmail.com>
2014-04-07 22:21:21 +0200

    Finished Lab3

--- lab3/Kruskal.rb
+++ lab3/Kruskal.rb
@@ -1,57 +1,49 @@
 class UnionFind
   def initialize
     @leaders = {}
-  end
-
-  def add city
-  	@leaders[city] = city
+    @leaders.default_proc = proc{ |hash, key| hash[key] = key }
   end
 
   def connected?(city1,city2)
     @leaders[city1] == @leaders[city2]
   end
 
-  def union(city1,city2)
+  def union(city1,city2)            
     leader_1, leader_2 = @leaders[city1], @leaders[city2]
     @leaders.each { |city, leader| @leaders[city] = leader_2 if (leader == leader_1)}
   end
 end
 
-def file
-  @file ||= File.readlines("edges.txt")
-end
-
 def run_kruskal
   set = UnionFind.new
+  minimum_spanning_tree = []
+  edges = []
 
-  @minimum_spanning_tree = []
-
-  edges = file.drop(1).map { |x| x.gsub(/\n/, "").split(" ").map(&:to_i) }.
-    map { |one, two, weight| { :from => one, :to => two, :weight => weight}}.
-    sort_by { |x| x[:weight]}
+  @file.each do |line|
+    if line.include? '--'
+      weight = line.slice!(/ \[\d+\]/).slice(/\d+/).to_i
+      cities = line.split("--")
+      city1 = cities.first
+      city2 = cities.last.gsub(/\n/, "")
+      edges << {from: city1, to: city2, weight: weight}
+    end
+  end
+  edges.sort_by! { |x| x[:weight]}
 
   edges.each do |edge|
     if !set.connected?(edge[:from], edge[:to])
-      @minimum_spanning_tree << edge
+      minimum_spanning_tree << edge
       set.union(edge[:from], edge[:to])
     end
   end
 
-  puts "MST: #{@minimum_spanning_tree}"
-  puts "Cost: #{@minimum_spanning_tree.inject(0) { |acc, x| acc + x[:weight]}}"
+  #puts "MST: #{minimum_spanning_tree}"
+  puts "Cost: #{minimum_spanning_tree.inject(0) { |res, x| res + x[:weight]}}"
 end
 
-def read filename
-  file = File.new(filename)
-  while line = file.gets
-    if line.matches(//)
-    	
-    end
-  end
-  puts "Datastructure is filled"
+def read filepath
+  @file ||= File.readlines(filepath)
 end
 
-path = '../testdata/lab3/USA-highway-miles.in'
-
-
-
+read '../testdata/lab3/USA-highway-miles.in'
+run_kruskal
