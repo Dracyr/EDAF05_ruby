f17a4bb8053e21b446779824e75d4c2f152770bb
Per-Victor Persson <dracyr@gmail.com>
2014-05-05 21:43:59 +0200

    Lab4

--- lab4/conq.rb
+++ lab4/conq.rb
@@ -7,20 +7,16 @@ class Point
     self.y = y.to_f
   end
 
-  def to_s
-    "#{id}, X:#{self.x}, Y:#{self.y}"
+  def distance_to(point)
+    Math.hypot(self.x - point.x, self.y - point.y)
   end
-end
-
-def distance(p1, p2)
-  Math.hypot(p1.x - p2.x, p1.y - p2.y)
 end
 
 def closest_bruteforce(points)
   mindist, minpts = Float::MAX, []
   points.length.times do |i|
     (i+1).upto(points.length - 1) do |j|
-      dist = distance(points[i], points[j])
+      dist = points[i].distance_to points[j]
       if dist < mindist
         mindist = dist
         minpts = [points[i], points[j]]
@@ -34,34 +30,37 @@ def closest_recursive(points)
   if points.length <= 3
     return closest_bruteforce(points)
   end
-  xP = points.sort_by {|p| p.x}
-  mid = (points.length / 2.0).ceil
-  pL = xP[0,mid]
-  pR = xP[mid..-1]
-  dL, pairL = closest_recursive(pL)
-  dR, pairR = closest_recursive(pR)
-  if dL < dR
-    dmin, dpair = dL, pairL
+  points.sort_by! {|p| p.x}
+  mid = (points.length / 2)
+  points_left = points[0,mid]
+  points_right = points[mid..-1]
+
+  min_left, pair_left = closest_recursive(points_left)
+  min_right, pair_right = closest_recursive(points_right)
+
+  if min_left < min_right
+    min_dist, min_pair = min_left, pair_left
   else
-    dmin, dpair = dR, pairR
+    min_dist, min_pair = min_right, pair_right
   end
-  yP = xP.find_all {|p| (pL[-1].x - p.x).abs < dmin}.sort_by {|p| p.y}
+
+  yP = points.find_all {|p| (points_left[-1].x - p.x).abs < min_dist}.sort_by {|p| p.y}
   closest = Float::MAX
   closestPair = []
   0.upto(yP.length - 2) do |i|
     (i+1).upto(yP.length - 1) do |k|
-      break if (yP[k].y - yP[i].y) >= dmin
-      dist = distance(yP[i], yP[k])
+      break if (yP[k].y - yP[i].y) >= min_dist
+      dist = yP[i].distance_to yP[k]
       if dist < closest
         closest = dist
         closestPair = [yP[i], yP[k]]
       end
     end
   end
-  if closest < dmin
+  if closest < min_dist
     [closest, closestPair]
   else
-    [dmin, dpair]
+    [min_dist, min_pair]
   end
 end
 
@@ -84,14 +83,3 @@ result = closest_recursive(points)
 #ps = result[1]
 #puts "Points for '#{path.split('/').last}': [#{ps[0].to_s}],[#{ps[1].to_s}], Distance: #{result[0]}"
 puts "../data/#{path.split('/').last}: #{points.length} #{result[0]}"
-
-#p ans1 = closest_bruteforce(points)
-#p ans2 = closest_recursive(points)
-#puts "hoppsan!" if ans1[0] != ans2[0]
-
-#require 'benchmark'
-#points = Array.new(10000) {Point.new(1, rand, rand)}
-#Benchmark.bm(12) do |x|
-#  x.report("bruteforce") {ans1 = closest_bruteforce(points)}
-#  x.report("recursive")  {ans2 = closest_recursive(points)}
-#end
