625c36a2013aa8890038b5d50ed174ebffc11d84
Per-Victor Persson <dracyr@gmail.com>
2014-05-13 21:47:34 +0200

    Lab5

--- lab5/lab5_print.rb
+++ lab5/lab5_print.rb
@@ -1,3 +1,5 @@
+#This version uses the out file to print in the correct order
+
 @blosum_matrix = [ [ 4, -1, -2, -2, 0, -1, -1, 0, -2, -1, -1, -1, -1, -2, -1, 1,
             0, -3, -2, 0, -2, -1, 0 ],
         [ -1, 5, 0, -2, -3, 1, 0, -2, 0, -3, -2, 2, -1, -3, -2, -1, -1,
