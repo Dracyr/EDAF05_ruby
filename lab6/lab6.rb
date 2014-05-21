8b81b19bdb2bfab7b288e747c616f1029fc40fd0
Per-Victor Persson <dracyr@gmail.com>
2014-05-21 08:45:09 +0200

    Lab6, max flow works, not min cut

--- lab6/lab6.rb
+++ lab6/lab6.rb
@@ -8,47 +8,23 @@
 #path = "/usr/local/cs/edaf05/lab6/rail.txt"
 path = "../testdata/lab6/rail.txt"
 
-@nodes = Hash.new { |hash, key| hash[key] = {} }
+@nodes = Hash.new { |hash, key| hash[key] = [] }
 @flow  = Hash.new { |hash, key| hash[key] = {} }
 @names = []
 @capacity = Hash.new { |hash, key| hash[key] = {} }
 
 INFINITY = 1073741823
-class Edge
-  attr_accessor :source, :sink, :capacity, :back_edge
-
-  def initialize source, sink, capacity
-    self.source = source
-    self.sink = sink
-    if capacity == -1
-      self.capacity = INFINITY #Integer max
-    else
-      self.capacity = capacity
-    end
-  end
-
-  def eql? other
-    self.source    == other.source &&
-    self.sink      == other.sink &&
-    self.capacity  == other.capacity &&
-    self.back_edge == other.back_edge
-  end
-
-  def hash
-    [self.source, self.sink].hash
-  end
-end
 
 def add_edge source, sink, capacity
-  @nodes[source].merge!({ sink => capacity })
-  @nodes[sink].merge!({ source => capacity })
+  @nodes[source] << sink
+  @nodes[sink] << source
 
   @flow[source].merge!({ sink => 0 })
   @flow[sink].merge!({ source => 0 })
 
   capacity = 1073741823 if capacity == -1 #Max int
   @capacity[source].merge!({sink => capacity})
-  @capacity[sink].merge!({source => capacity}) 
+  @capacity[sink].merge!({source => capacity})
 end
 
 def parse filepath
@@ -64,22 +40,6 @@ def parse filepath
   end
 end
 
-def find_path source, sink, path
-  return [] if source == sink
-  naj = @nodes[source]
-  marked = []
-  @nodes[source].each do |edge|
-    residual = edge.capacity - @flow[edge]
-    if residual > 0 && !marked.include?(edge.sink)
-      marked << edge.sink
-      new_path = path + [edge]
-      result = find_path( edge.sink, sink, new_path )
-      return result unless result.empty?
-    end
-  end
-  []
-end
-
 def trace from, to, parent
   return [] unless parent[to]
   path = [to]
@@ -92,64 +52,77 @@ def trace from, to, parent
   path
 end
 
-def augmenting_path from, to
-  return 0 if from == to
-  queue = [from]
-  discovered = [from]
+def augmenting_path source, sink
+  return 0 if source == sink
+  queue = [source]
+  visited = [source]
   parent = {}
   until queue.empty?
     node = queue.shift
-    edges = @nodes[node].keys
-    edges -= discovered
+    edges = @nodes[node]
+    edges -= visited
     unless edges.empty?
-      if edges.include? to
-        parent.merge!( { to => node } )
-        break
-      else
-        discovered += edges
-        queue.push(*edges)
-        edges.each { |e| parent.merge!({e => node}) }
+      edges.each do |e|
+        residual = @capacity[node][e] - @flow[node][e]
+        if residual > 0
+          visited << e
+          queue.push(e)
+          parent.merge!({e => node})
+          if e == sink
+            break
+          end
+        end
       end
     end
   end
-  trace from, to, parent
+  #parent
+  trace source, sink, parent
 end
 
 def max_flow source, sink
   max_flow = 0
+  min_cut = []
   path = augmenting_path source, sink
   until path.empty?
-    residuals = []
 
+    flow_cap = INFINITY
+    bottleneck = []
     0.upto( path.length - 2 ) do |i|
       u, v = path[i], path[i+1]
-      residuals << @capacity[u][v] - @flow[u][v]
+      residual = @capacity[u][v] - @flow[u][v]
+      if residual < flow_cap
+        flow_cap = residual
+        bottleneck = [u,v, @capacity[u][v]]
+      end
     end
-    flow_min = residuals.min
-
-    max_flow += flow_min
-    puts
-    if flow_min == 0
-      puts "fucked"
-      break
-    end
+    max_flow += flow_cap
+    #asd = bottleneck << flow_cap
+    min_cut << bottleneck
 
     where = sink
     (path.length - 1).downto(1) do |i|
       prev = path[i - 1]
-      #puts "#{i}:#{front}:#{back}"
-      #puts @flow[front][back]      
-      @flow[prev][where] -= flow_min
-      @flow[where][prev] += flow_min
+      @flow[prev][where] += flow_cap
+      @flow[where][prev] -= flow_cap
       where = prev
     end
     path = augmenting_path source, sink
   end
-  [@max_flow, path]
+  [max_flow, min_cut]
   #@nodes[source].inject(0) { |sum, edge| sum + @flow[edge] }
+end
+
+def min_cut_stuff
+  @nodes.each_pair do |key, value|
+
+
+  end
+
 end
 
 parse path
 
-max_flow 0, 54
-
+result = max_flow 0, 54
+puts "Max flow: #{result.first}"
+min_cut = result.last
+min_cut.each { |c| puts c.inspect }
